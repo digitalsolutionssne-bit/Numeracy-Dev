fae61d52672cce010bbde06f6558bd26a7bef0bc
digitalsolutionssne-bit <digitalsolutionssne@gmail.com>
2026-04-01 08:00:15 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lifecount-cache-v8';
+const CACHE_NAME = 'lifecount-cache-v9';
 
 // All the core files needed for the app to function 100% offline
 const urlsToCache =[
@@ -12,7 +12,9 @@ const urlsToCache =[
     './pages/stopwatch.html',
     './pages/duration.html',
     './pages/end-time.html',
-    './pages/expiry.html'
+    './pages/expiry.html',
+    './assets/icon-192.png',
+    './assets/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
